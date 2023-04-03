360f7e2901a2ba86df72aadb414330d869e20a85
Mahzabin <mashrufa1997@yahoo.co.uk>
2023-04-03 22:35:54 +0100

    created a simple application

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+# creating variables of different types
+todays_workout = "leg day"
+leg_press_in_kilos = 25
+lunges_per_rep = 10
+
+# call the print function using the variables and some operators
+# also using str() function to convert integers to strings
+print("Today is " + str(todays_workout) + ". We will focus on leg press, which will be " + str(leg_press_in_kilos) + " kilos, 10 reps for 3 sets." + " And finally " + str(lunges_per_rep) + " lunges per rep for 3 sets will finish the workout.")
+
+# when running python3 precourse_recap.py it returns:
+# Today is leg day. We will focus on leg press, which will be 25 kilos, 10 reps for 3 sets. And finally 10 lunges per rep for 3 sets will finish the workout.
